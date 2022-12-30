7a027404bcc602be7da0f8f7635111aba194ca37
황정현 <jhhwang@wemakeprice.com>
2022-12-30 16:18:11 +0900

    study 하루에 1 - 2문제 풀이 (오타수정)

--- src/test/test03/Test3.java
+++ src/test/test03/Test3.java
@@ -12,7 +12,31 @@ public class Test3 {
      *
      * 합이 6이 되는 연속부분수열은 {2, 1, 3}, {1, 3, 1, 1}, {3, 1, 1, 1}로 총 3가지입니다.
      */
+
+    public static int solution() {
+        int answer = 0;
+        int[] arr = {1, 2, 1, 3, 1, 1, 1, 2};
+        int specialNumber = 6;
+        int lt = 0;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if (sum == specialNumber) {
+                answer ++;
+            }
+            while (sum > specialNumber) {
+                sum -= arr[lt++];
+
+                if (sum == specialNumber) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args) {
+        System.out.println(solution());
 
         int[] arr = {1, 2, 1, 3, 1, 1, 1, 2};
         int m = 6;
