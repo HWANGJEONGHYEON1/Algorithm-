45b66152fb396522b4b5893a80cd7b28b68f2cba
jhhwnag <jhhwang@wemakeprice.com>
2023-01-06 18:16:09 +0900

    study- 1~2문제 알고리즘 스택 - 2

---
+++ src/test/test03/Test12.java
@@ -0,0 +1,39 @@
+package test.test03;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+// 매출액의 종류
+public class Test12 {
+    /**
+     * 입력된 문자열에서 소괄호 ( ) 사이에 존재하는 모든 문자를 제거하고 남은 문자만 출력하는 프로그램을 작성하세요.
+     *
+     * (A(BC)D)EF(G(H)(IJ)K)LM(N)
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(solution());
+    }
+
+    private static String solution() {
+        String answer = "";
+        String input = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+        Stack<String> stack = new Stack<>();
+
+        String[] split = input.split("");
+        for (String s : split) {
+            if (")".equals(s)) {
+                while (!stack.pop().equals("(")) {
+                }
+            } else {
+                stack.push(s);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            answer = stack.pop() + answer;
+        }
+        return answer;
+    }
+}
