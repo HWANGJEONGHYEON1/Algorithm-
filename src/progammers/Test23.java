6acda92e4c53bb03f70a7e17740092c7dc1887c0
jhhwnag <jhhwang@wemakeprice.com>
2023-02-07 11:23:40 +0900

    study 1 -2 문제 알고리즘

---
+++ src/progammers/Test23.java
@@ -0,0 +1,31 @@
+package progammers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+// 두 개 뽑아서 더하기
+public class Test23 {
+
+    public static void main(String[] args) {
+        new Test23().solution(new int[] {2,1,3,4,1}); // [2,3,4,5,6,7]
+        new Test23().solution(new int[] {5,0,2,7});
+    }
+
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        Set<Integer> set = new TreeSet<>();
+
+        for (int i = 0; i < numbers.length; i++){
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        System.out.println(set);
+
+        return set.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+}
